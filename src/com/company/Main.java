3487b9607198ef90aca446036045a06db5b0d3a6
AlexR1th <68252472+AlexR1th@users.noreply.github.com>
2020-07-28 19:05:04 +0300

    Update Main.java
    lollllll

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String hello = "Hello OOP / good bey!";
+	String hello = "Hello OOP / good bie!!!!!!!!!!!";
         System.out.println(hello);
         
     }
