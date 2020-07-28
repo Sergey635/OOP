1850446566b66816b458b1809592647431302833
AlexR1th <wandering123456@gmail.com>
2020-07-28 19:04:05 +0300

    lol

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	String hello = "Hello OOP";
+	String hello = "Hello OOP / good bey!";
         System.out.println(hello);
         
     }
